468ac22bff48e35664db00694a8f1bfbcafd894f
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-29 21:18:42 +0100

    feat: modified createUser method of UserController to allow for user creation (country is temporarily set for a random default value)

--- src/main/java/hu/okrim/productreviewappcomplete/controller/UserController.java
+++ src/main/java/hu/okrim/productreviewappcomplete/controller/UserController.java
@@ -2,14 +2,15 @@ package hu.okrim.productreviewappcomplete.controller;
 
 import hu.okrim.productreviewappcomplete.dto.UserDTO;
 import hu.okrim.productreviewappcomplete.mapper.UserMapper;
-import hu.okrim.productreviewappcomplete.model.User;
-import hu.okrim.productreviewappcomplete.service.CountryService;
+import hu.okrim.productreviewappcomplete.model.Country;
 import hu.okrim.productreviewappcomplete.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping("/user")
@@ -23,7 +24,10 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<HttpStatus> createUser(@RequestBody UserDTO user) {
+    public ResponseEntity<HttpStatus> createUser(@Valid @RequestBody UserDTO user) {
+        user.setIsActive(true);
+        user.setRegistrationDate(ZonedDateTime.now());
+        user.setCountry(new Country("HUN", "Hungary"));
         userService.saveUser(UserMapper.mapToUser(user));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
