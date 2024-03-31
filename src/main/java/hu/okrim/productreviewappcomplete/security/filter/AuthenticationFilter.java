e35a66988085e5295fb720891e7c33d0b06f2a73
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-03-31 18:27:19 +0200

    feat: exposed Authorization header in response so that I can save it in the frontend

--- src/main/java/hu/okrim/productreviewappcomplete/security/filter/AuthenticationFilter.java
+++ src/main/java/hu/okrim/productreviewappcomplete/security/filter/AuthenticationFilter.java
@@ -55,6 +55,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
             .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
         response.addHeader(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER + token);
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
     }
 
 
