d2260d8c5598c329e175ef2c236b41cc6414d3e7
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-04-01 22:31:30 +0200

    fix: added missing annotation

--- src/main/java/hu/okrim/productreviewappcomplete/repository/CountryRepository.java
+++ src/main/java/hu/okrim/productreviewappcomplete/repository/CountryRepository.java
@@ -2,9 +2,10 @@ package hu.okrim.productreviewappcomplete.repository;
 
 import hu.okrim.productreviewappcomplete.model.Country;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CountryRepository extends CrudRepository<Country, String> {
     List<Country> findByOrderByNameAsc();
 }
