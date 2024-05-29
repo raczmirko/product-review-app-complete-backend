7223e3d101515a0df53786c6c73f331d93c9fdb5
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-05-29 18:02:36 +0200

    feat: created createOrModifyProductCharacteristicValue method

--- src/main/java/hu/okrim/productreviewappcomplete/controller/ProductCharacteristicValueController.java
+++ src/main/java/hu/okrim/productreviewappcomplete/controller/ProductCharacteristicValueController.java
@@ -7,10 +7,9 @@ import hu.okrim.productreviewappcomplete.service.ProductCharacteristicsValueServ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product-characteristic-value")
@@ -18,14 +17,27 @@ public class ProductCharacteristicValueController {
     @Autowired
     ProductCharacteristicsValueService pcvService;
     @PostMapping("/create")
-    public ResponseEntity<?> createProductCharacteristicValue(@RequestBody ProductCharacteristicValueDTO pcvDTO){
+    public ResponseEntity<?> createOrModifyProductCharacteristicValue(@RequestBody ProductCharacteristicValueDTO pcvDTO){
         ProductCharacteristicValue pcv = ProductCharacteristicValueMapper.mapToProductCharacteristicValue(pcvDTO);
+        ProductCharacteristicValue updatedPcv = pcvService.findByProductAndCharacteristic(pcv.getProduct(), pcv.getCharacteristic());
         try {
-            pcvService.save(pcv);
+            if(updatedPcv != null){
+                updatedPcv.setValue(pcv.getValue());
+                pcvService.save(updatedPcv);
+            }
+            else {
+                pcvService.save(pcv);
+            }
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
         catch (Exception ex) {
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
         }
+    }
+
+    @GetMapping("/{id}/all")
+    public ResponseEntity<?> findProductCharacteristicValuesByProduct(@PathVariable("id") Long id){
+        List<ProductCharacteristicValue> pcvs = pcvService.findByProductId(id);
+        return new ResponseEntity<>(pcvs, HttpStatus.OK);
     }
 }
