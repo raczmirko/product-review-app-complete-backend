3bfcd053b5e35d1683ac9e36b9f06a161832a853
Rácz Mirkó <123543210+raczmirko@users.noreply.github.com>
2024-06-16 16:14:58 +0200

    feat: changed error text on unauthorized action

--- src/main/java/hu/okrim/productreviewappcomplete/util/AuthorizationUtil.java
+++ src/main/java/hu/okrim/productreviewappcomplete/util/AuthorizationUtil.java
@@ -15,7 +15,7 @@ public class AuthorizationUtil {
     public ResponseEntity<?> checkAuthorization(HttpServletRequest request, String requestUsername) {
         String tokenUsername = jwtUtil.extractUserFromToken(request);
         if (!requestUsername.equals(tokenUsername)) {
-            String errorMessage = "UNAUTHORIZED: This review belongs to another user.";
+            String errorMessage = "UNAUTHORIZED: The entity you are trying to modify belongs to another user.";
             return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
         }
         return null;
